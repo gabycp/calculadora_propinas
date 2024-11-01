eda540f1272ae135c56fd25d7baef391b2f6772e
Gabriela Alejandra Cornejo <gaby_cor93@hotmail.com>
2024-11-01 04:55:10 -0600

    save items initial

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,11 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItem, OrderItem } from "../types"
 
 export default function useOrder(){
     
     const [order, setOrder] = useState<OrderItem[]>([])
 
-    const AddItem = () => {
+    const AddItem = (item: MenuItem) => {
         console.log("Agregando...");
         
     }
